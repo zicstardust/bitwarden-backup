8bedd0e4db5a587b4db1e56dbd22df1e33d364b0
Zic <git@zicstardust.com>
2025-01-24 16:26:55 -0400

    update native

---
+++ src/download_powershell.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Last LTS - https://aka.ms/powershell-release?tag=lts
+#Last Stable - https://aka.ms/powershell-release?tag=stable
+POWERSHELL_VERSION="7.4.7"
+
+
+arch=$(lscpu | grep Architecture)
+if [[ $arch == *"x86_64"* ]]; then
+    POWERSHELL_ARCH="x64"
+elif [[ $arch == *"arm64"* ]]; then
+    POWERSHELL_ARCH="arm64"
+else
+    exit 1
+fi
+
+wget https://github.com/PowerShell/PowerShell/releases/download/v${POWERSHELL_VERSION}/powershell-${POWERSHELL_VERSION}-linux-${POWERSHELL_ARCH}.tar.gz -O powershell.tar.gz
